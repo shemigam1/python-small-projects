d5bb81eea7ce96b0adeb42ef3e28050364cde3fe
semil <semilore@omotade-michaels.com>
2023-01-03 14:52:07 +0100

    added more questions

--- quiz_game.py
+++ quiz_game.py
@@ -14,4 +14,18 @@ answer = answer.lower()
 if answer == 'central processing unit':
     print('correct, 10 points to Gryffndor!')
 else:
-    print("Wrong! try again")
+    print("Wrong! try again... its central processing unit")
+
+answer = input("what is a bad hacker called? ")
+answer = answer.lower()
+if answer == 'black hat hacker':
+    print('correct, 10 points to Gryffndor!')
+else:
+    print("Wrong! try again... its black hat hacker")
+
+answer = input("who created the linux kernel? ")
+answer = answer.lower()
+if answer == 'linus torvalds':
+    print('correct, 10 points to Gryffndor!')
+else:
+    print("Wrong! try again... linus torvalds")
